4a335bb9bb2c598f732a6a8a43f254d8956cde02
Mayur <hedaumayur2003@gmail.com>
2025-11-14 22:55:46 +0530

    Day 9 Complete

--- Day9/src/App.jsx
+++ Day9/src/App.jsx
@@ -1,8 +1,59 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
   const [active, setActive] = useState("metric");
+  const [BMI, setBMI] = useState(0);
+  const [weight, setweight] = useState(0);
+  const [height, setheight] = useState(0);
+  const [category, setcategory] = useState("");
+  const handleBMI=()=>{
+    if(active==="metric")
+    {
+      setBMI(
+  Number(
+    (Math.floor((weight / ((height / 100) * (height / 100))) * 100) / 100)
+      .toFixed(2)
+  )
+);
+
+    }
+    else{
+      setBMI(
+  Number(
+    (Math.floor(((weight * 703) / (height * height)) * 10) / 10)
+      .toFixed(1)
+  )
+);
+
+    }
+  }
+  const getCategory=()=>{
+    if(BMI==0)
+    {
+      setcategory("");
+      return;
+    }
+    if(BMI<18.5)
+    {
+      setcategory("underweight");
+    }
+    else if(BMI<24.9)
+    {
+      setcategory("normal");
+    }
+    else if(BMI<29.9)
+    {
+      setcategory("overweight");
+    }
+    else{
+      setcategory("obesity");
+    }
+  }
+  useEffect(() => {
+    
+    getCategory();
+  }, [BMI]);
 
   return (
     <div className="min-h-screen bg-gradient-to-br from-blue-200 to-purple-300 flex justify-center items-center p-5">
@@ -53,7 +104,9 @@ function App() {
             </label>
             <input
               type="number"
+              value={weight}
               className="w-full p-3 mt-1 bg-gray-100 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-400 outline-none"
+              onChange={(e)=>setweight(e.target.value)}
             />
           </div>
 
@@ -64,13 +117,15 @@ function App() {
             </label>
             <input
               type="number"
+              value={height}
               className="w-full p-3 mt-1 bg-gray-100 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-400 outline-none"
+              onChange={(e)=>setheight(e.target.value)}
             />
           </div>
 
           {/* Buttons */}
           <div className="mt-8 flex justify-between">
-            <button className="w-[60%] bg-blue-600 hover:bg-blue-700 text-white py-3 rounded-xl shadow-lg text-lg font-semibold transition">
+            <button className="w-[60%] bg-blue-600 hover:bg-blue-700 text-white py-3 rounded-xl shadow-lg text-lg font-semibold transition" onClick={handleBMI}>
               Calculate BMI
             </button>
 
@@ -84,13 +139,20 @@ function App() {
         <div className="bg-white w-full md:w-[50%] shadow-xl rounded-2xl p-8 text-center">
           <p className="text-gray-600 text-lg font-medium">Your BMI Score</p>
 
-          <h1 className="text-6xl font-bold text-gray-800 mt-3">22.9</h1>
+          <h1 className="text-6xl font-bold text-gray-800 mt-3">{BMI}</h1>
 
           {/* Status Badge */}
           <div className="flex justify-center items-center">
-            <div className="w-48 h-12 border border-green-500 bg-white text-green-600 text-center font-semibold flex justify-center items-center rounded-full shadow mt-5">
-            Normal Weight
-          </div>
+            <div className={`w-48 h-12 text-center font-semibold flex justify-center items-center 
+rounded-full shadow mt-5
+${category === "underweight" ? "bg-yellow-200 text-yellow-700" : ""}
+${category === "normal" ? "bg-green-200 text-green-700" : ""}
+${category === "overweight" ? "bg-orange-200 text-orange-700" : ""}
+${category === "obesity" ? "bg-red-200 text-red-700" : ""}
+`}>
+  {category ? category.toUpperCase() : "No Result"}
+</div>
+
           </div>
 
           {/* Categories */}
@@ -99,24 +161,24 @@ function App() {
           </p>
 
           <div className="flex justify-center items-center mt-5 gap-8 text-gray-700">
-            <div className="bg-gray-100 p-4 rounded-xl shadow-md w-32">
+            <div className={`bg-gray-100 p-4 rounded-xl shadow-md w-32 ${category==="underweight" ? "bg-green-200" : ""}`}>
               <p className="font-semibold">Underweight</p>
               <p className="text-sm">&lt; 18.5</p>
             </div>
 
-            <div className="bg-gray-100 p-4 rounded-xl shadow-md w-32">
+            <div className={`bg-gray-100 p-4 rounded-xl shadow-md w-32 ${category==="normal" ? "bg-green-200" : ""}`}>
               <p className="font-semibold">Normal</p>
               <p className="text-sm">18.5 - 24.9</p>
             </div>
           </div>
 
           <div className="flex justify-center items-center mt-4 gap-8 text-gray-700">
-            <div className="bg-gray-100 p-4 rounded-xl shadow-md w-32">
+            <div className={`bg-gray-100 p-4 rounded-xl shadow-md w-32 ${category==="overweight" ? "bg-green-200" : ""}`}>
               <p className="font-semibold">Overweight</p>
               <p className="text-sm">25 - 29.9</p>
             </div>
 
-            <div className="bg-gray-100 p-4 rounded-xl shadow-md w-32">
+            <div className={`bg-gray-100 p-4 rounded-xl shadow-md w-32 ${category==="obesity" ? "bg-green-200" : ""}`}>
               <p className="font-semibold">Obesity</p>
               <p className="text-sm">&gt;= 30</p>
             </div>
